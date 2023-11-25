0f3d91d9c7dd9bacc77d8450435bf9e90385626b
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-11-25 12:09:00 +0200

    added some details

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,3 @@
 import logo from "./logo.svg";
-
-export { logo };
+import yellowshutter from "./yellowshutter.png";
+export { logo, yellowshutter };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,30 @@ import { styles } from "../styles";
 import { navLinks } from "../constants";
 import { logo } from "../assets";
 const Navbar = () => {
-    return <div>Navbar</div>;
+    const [active, setActive] = useState("");
+
+    return (
+        <nav
+            className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-secondary `}
+        >
+            <div className="w-full flex justify-between items-center max-w-7xl mx-auto ">
+                <Link
+                    to="/"
+                    className="flex item-center gap-2"
+                    onClick={() => {
+                        setActive("");
+                        window.scrollTo(0, 0);
+                    }}
+                >
+                    <img
+                        src={logo}
+                        alt="logo"
+                        className="w-9 h-9 object-contain  "
+                    />
+                </Link>
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
