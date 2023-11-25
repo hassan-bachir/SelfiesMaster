01c9b4270bad8a10897e4f63573c26368f893ed9
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-11-25 12:43:54 +0200

    changed the navbar styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
 
     return (
         <nav
-            className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-secondary `}
+            className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-tertiary `}
         >
             <div className="w-full flex justify-between items-center max-w-7xl mx-auto ">
                 <Link
