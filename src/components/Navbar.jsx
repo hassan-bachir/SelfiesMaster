29ec79ea269dc2e75cccb538cfb924d7c0d32954
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-11-27 16:41:09 +0200

    updatad the main navigation bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
 
     return (
         <nav
-            className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary `}
+            className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-tertiary `}
         >
             <div className="w-full flex justify-between items-center max-w-7xl mx-auto ">
                 <Link
@@ -25,9 +25,32 @@ const Navbar = () => {
                         className="w-36 object-contain  "
                     />
                 </Link>
+                <ul className="list-none hidden sm:flex flex-row gap-10">
+                    {navLinks.map((link) => (
+                        <li
+                            key={link.id}
+                            className={`${
+                                active === link.title
+                                    ? "text-white"
+                                    : "text-primary"
+                            } hover:text-white text-[18px] font-medium cursor-pointer `}
+                            onClick={() => setActive(link.title)}
+                        >
+                            <a href={`#${link.id}`}>{link.title}</a>
+                        </li>
+                    ))}
+                </ul>
             </div>
         </nav>
     );
 };
 
 export default Navbar;
+
+// <ul className="list-none hidden sm:flex flex-row gap-10">
+//     {navLinks.map((link) => {
+//         <li key={link.id}>
+//             <a href={`# ${link.id}`}>{link.title}</a>
+//         </li>;
+//     })}
+// </ul>
