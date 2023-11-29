7765ddcc0a523a56e6239ff749c7d1a6a6847aec
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-11-29 12:58:25 +0200

    added dummy text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,165 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+    return (
+        <div>
+            <nav>Navigation bar</nav>
+            <div>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Praesent vel tellus eget enim ullamcorper semper sed ac
+                    magna. Donec auctor, enim sed ullamcorper semper, eros
+                    lectus elementum ligula, eget eleifend quam tortor in
+                    lectus. Nullam id quam eu quam hendrerit viverra nec quis
+                    velit. Nullam ac mauris et elit semper tincidunt ac eget
+                    quam. Vivamus ac magna vel eros iaculis faucibus. Nullam sit
+                    amet eros eget mi aliquam luctus.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Praesent vel tellus eget enim ullamcorper semper sed ac
+                    magna. Donec auctor, enim sed ullamcorper semper, eros
+                    lectus elementum ligula, eget eleifend quam tortor in
+                    lectus. Nullam id quam eu quam hendrerit viverra nec quis
+                    velit. Nullam ac mauris et elit semper tincidunt ac eget
+                    quam. Vivamus ac magna vel eros iaculis faucibus. Nullam sit
+                    amet eros eget mi aliquam luctus.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Praesent vel tellus eget enim ullamcorper semper sed ac
+                    magna. Donec auctor, enim sed ullamcorper semper, eros
+                    lectus elementum ligula, eget eleifend quam tortor in
+                    lectus. Nullam id quam eu quam hendrerit viverra nec quis
+                    velit. Nullam ac mauris et elit semper tincidunt ac eget
+                    quam. Vivamus ac magna vel eros iaculis faucibus. Nullam sit
+                    amet eros eget mi aliquam luctus.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Praesent vel tellus eget enim ullamcorper semper sed ac
+                    magna. Donec auctor, enim sed ullamcorper semper, eros
+                    lectus elementum ligula, eget eleifend quam tortor in
+                    lectus. Nullam id quam eu quam hendrerit viverra nec quis
+                    velit. Nullam ac mauris et elit semper tincidunt ac eget
+                    quam. Vivamus ac magna vel eros iaculis faucibus. Nullam sit
+                    amet eros eget mi aliquam luctus.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Praesent vel tellus eget enim ullamcorper semper sed ac
+                    magna. Donec auctor, enim sed ullamcorper semper, eros
+                    lectus elementum ligula, eget eleifend quam tortor in
+                    lectus. Nullam id quam eu quam hendrerit viverra nec quis
+                    velit. Nullam ac mauris et elit semper tincidunt ac eget
+                    quam. Vivamus ac magna vel eros iaculis faucibus. Nullam sit
+                    amet eros eget mi aliquam luctus.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Praesent vel tellus eget enim ullamcorper semper sed ac
+                    magna. Donec auctor, enim sed ullamcorper semper, eros
+                    lectus elementum ligula, eget eleifend quam tortor in
+                    lectus. Nullam id quam eu quam hendrerit viverra nec quis
+                    velit. Nullam ac mauris et elit semper tincidunt ac eget
+                    quam. Vivamus ac magna vel eros iaculis faucibus. Nullam sit
+                    amet eros eget mi aliquam luctus.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Praesent vel tellus eget enim ullamcorper semper sed ac
+                    magna. Donec auctor, enim sed ullamcorper semper, eros
+                    lectus elementum ligula, eget eleifend quam tortor in
+                    lectus. Nullam id quam eu quam hendrerit viverra nec quis
+                    velit. Nullam ac mauris et elit semper tincidunt ac eget
+                    quam. Vivamus ac magna vel eros iaculis faucibus. Nullam sit
+                    amet eros eget mi aliquam luctus.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Praesent vel tellus eget enim ullamcorper semper sed ac
+                    magna. Donec auctor, enim sed ullamcorper semper, eros
+                    lectus elementum ligula, eget eleifend quam tortor in
+                    lectus. Nullam id quam eu quam hendrerit viverra nec quis
+                    velit. Nullam ac mauris et elit semper tincidunt ac eget
+                    quam. Vivamus ac magna vel eros iaculis faucibus. Nullam sit
+                    amet eros eget mi aliquam luctus.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Praesent vel tellus eget enim ullamcorper semper sed ac
+                    magna. Donec auctor, enim sed ullamcorper semper, eros
+                    lectus elementum ligula, eget eleifend quam tortor in
+                    lectus. Nullam id quam eu quam hendrerit viverra nec quis
+                    velit. Nullam ac mauris et elit semper tincidunt ac eget
+                    quam. Vivamus ac magna vel eros iaculis faucibus. Nullam sit
+                    amet eros eget mi aliquam luctus.
+                </p>
 
-export default Hero
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Praesent vel tellus eget enim ullamcorper semper sed ac
+                    magna. Donec auctor, enim sed ullamcorper semper, eros
+                    lectus elementum ligula, eget eleifend quam tortor in
+                    lectus. Nullam id quam eu quam hendrerit viverra nec quis
+                    velit. Nullam ac mauris et elit semper tincidunt ac eget
+                    quam. Vivamus ac magna vel eros iaculis faucibus. Nullam sit
+                    amet eros eget mi aliquam luctus.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Praesent vel tellus eget enim ullamcorper semper sed ac
+                    magna. Donec auctor, enim sed ullamcorper semper, eros
+                    lectus elementum ligula, eget eleifend quam tortor in
+                    lectus. Nullam id quam eu quam hendrerit viverra nec quis
+                    velit. Nullam ac mauris et elit semper tincidunt ac eget
+                    quam. Vivamus ac magna vel eros iaculis faucibus. Nullam sit
+                    amet eros eget mi aliquam luctus.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Praesent vel tellus eget enim ullamcorper semper sed ac
+                    magna. Donec auctor, enim sed ullamcorper semper, eros
+                    lectus elementum ligula, eget eleifend quam tortor in
+                    lectus. Nullam id quam eu quam hendrerit viverra nec quis
+                    velit. Nullam ac mauris et elit semper tincidunt ac eget
+                    quam. Vivamus ac magna vel eros iaculis faucibus. Nullam sit
+                    amet eros eget mi aliquam luctus.
+                </p>
+
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Praesent vel tellus eget enim ullamcorper semper sed ac
+                    magna. Donec auctor, enim sed ullamcorper semper, eros
+                    lectus elementum ligula, eget eleifend quam tortor in
+                    lectus. Nullam id quam eu quam hendrerit viverra nec quis
+                    velit. Nullam ac mauris et elit semper tincidunt ac eget
+                    quam. Vivamus ac magna vel eros iaculis faucibus. Nullam sit
+                    amet eros eget mi aliquam luctus.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Praesent vel tellus eget enim ullamcorper semper sed ac
+                    magna. Donec auctor, enim sed ullamcorper semper, eros
+                    lectus elementum ligula, eget eleifend quam tortor in
+                    lectus. Nullam id quam eu quam hendrerit viverra nec quis
+                    velit. Nullam ac mauris et elit semper tincidunt ac eget
+                    quam. Vivamus ac magna vel eros iaculis faucibus. Nullam sit
+                    amet eros eget mi aliquam luctus.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Praesent vel tellus eget enim ullamcorper semper sed ac
+                    magna. Donec auctor, enim sed ullamcorper semper, eros
+                    lectus elementum ligula, eget eleifend quam tortor in
+                    lectus. Nullam id quam eu quam hendrerit viverra nec quis
+                    velit. Nullam ac mauris et elit semper tincidunt ac eget
+                    quam. Vivamus ac magna vel eros iaculis faucibus. Nullam sit
+                    amet eros eget mi aliquam luctus.
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
