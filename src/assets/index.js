447283c896677559b824537d804a3fec4a628b69
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-12-01 23:23:51 +0200

    added the video background

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,4 +2,5 @@ import logo from "./logo.svg";
 import yellowshutter from "./yellowshutter.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
-export { logo, yellowshutter, menu, close };
+import video from "./video.mp4";
+export { logo, yellowshutter, menu, close, video };
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,24 @@
 import { motion } from "framer-motion";
 import { styles } from "../styles";
-
+import { video } from "../assets";
 const Hero = () => {
     return (
-        <section className="relative w-full h-screen mx-auto bg-secondary"></section>
+        <motion.section
+            className="relative w-full h-screen mx-auto bg-secondary"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+        >
+            <video
+                autoPlay
+                loop
+                muted
+                className="absolute top-0 left-0 w-full h-full object-cover"
+            >
+                <source src={video} type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
+        </motion.section>
     );
 };
 
