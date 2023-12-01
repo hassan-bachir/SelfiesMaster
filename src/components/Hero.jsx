3b88d1663bcd5df6e152a3f195f1e53be677049b
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-12-01 22:52:44 +0200

    created the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
 
 const Hero = () => {
-    return <div></div>;
+    return (
+        <section className="relative w-full h-screen mx-auto bg-secondary"></section>
+    );
 };
 
 export default Hero;
